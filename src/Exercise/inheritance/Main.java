0c04f35608f498069e972e6b61206d6e889bbe42
Truong <vjc.ichi.149@gmail.com>
2022-01-12 16:20:12 +0700

    inheration

---
+++ src/Exercise/inheritance/Main.java
@@ -0,0 +1,8 @@
+package Exercise.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(2f, 3f);
+        System.out.println(point);
+    }
+}
