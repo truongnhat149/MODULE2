77492da86e47d9a261a415b5251b60f02de58ec0
Truong <vjc.ichi.149@gmail.com>
2022-01-12 17:24:19 +0700

    triangle - shape

--- src/Exercise/main.java
+++ src/Exercise/main.java
@@ -2,5 +2,22 @@ package Exercise;
 
 public class main {
     public static void main(String[] args) {
+
+
+            Animal animal = new Animal();
+
+            animal.speak();
+
+
+    }
+    public static class Animal {
+    public Animal() {}
+
+        private void speak(){
+
+            System.out.println("Hello");
+
+        }
+
     }
 }
